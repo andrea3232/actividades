50f8d0698e4ea753ec5426cc1cfc2f53107ee470
andrea3232 <andrea20453@cbtis75.edu.mx>
2022-10-05 07:56:26 -0500

    05/10/22

---
+++ hoy.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body background="img3.jpg">
+    <?php
+    echo '<center>';
+    echo '28/09/2022';
+    echo '</center>';
+    ?>
+    <center>
+        <img src="emoji1.jpg">
+    </center>
+</body>
+</html>
